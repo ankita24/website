b3ba426e2c47567c5f7dbb18e76c2ca4504d9d46
Ankita <riya.ankita24@gmail.com>
2023-07-22 13:09:09 +0530

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import potrait from "./potrait_pink.png";
+import LinkedIN from "./linkedIn.png";
+import Github from "./github.png";
+import Twitter from "./twitter.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="Container">
+        <div className="TitleBox">
+          <img src={potrait} style={{ width: 80, height: 80 }} alt="Ankita" />
+          <p className="Title">Ankita Panigrahi</p>
+        </div>
+        <div className="Container Navbar">
+          <div class="hover-underline-animation">About</div>
+          <div class="hover-underline-animation">Work Experience</div>
+          <div class="hover-underline-animation">Download Resume</div>
+          <div class="hover-underline-animation">Hobbies</div>
+          <div class="hover-underline-animation">Contact</div>
+        </div>
+      </div>
+      <div className="Content">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A Frontend focused Web Developer building the Frontend of Websites and
+          Web Applications that leads to the success of the overall product
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      </div>
+      <div className="Socials">
+        <a href='https://www.linkedin.com/in/ankita-panigrahi-4654917a/' target='_blank' rel="noreferrer">
+          <img src={LinkedIN} alt='linkedIn'/>
         </a>
-      </header>
+        <a href="https://github.com/ankita24" target='_blank' rel="noreferrer">
+        <img src={Github} alt='github'/>
+        </a>
+        <a href="https://twitter.com/ankita24_riya" target='_blank' rel="noreferrer">
+        <img src={Twitter} alt='twitter'/>
+        </a>
+      </div>
+      {/* 
+      <div className="FlexBox">
+        <div className="Element">ReactJS</div>
+        <div className="Element">Javascript</div>
+        <div className="Element">HTML</div>
+        <div className="Element">CSS</div>
+        <div className="Element">Typescript</div>
+        <div className="Element">NextJS</div>
+        <div className="Element">Git</div>
+      </div>
+      
+      <div style={{ textAlign: "center", marginTop: 50 }}>
+        <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 1 }}>
+          <button className="Button">Work Experience</button>
+        </motion.div>
+      </div> */}
     </div>
   );
 }
