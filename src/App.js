473eb8ae177bcb8660e7c418a63a28bd0d81548e
Ankita <riya.ankita24@gmail.com>
2023-07-31 19:51:23 +0530

    app

--- src/App.js
+++ src/App.js
@@ -2,57 +2,74 @@ import potrait from "./potrait_pink.png";
 import LinkedIN from "./linkedIn.png";
 import Github from "./github.png";
 import Twitter from "./twitter.png";
-import "./App.css";
+import useCheckPlatform from "./hooks/useCheckPlatform";
+import "./App.scss";
 
 function App() {
+  const isMobile = useCheckPlatform();
   return (
     <div className="App">
-      <div className="Container">
+      <div className={isMobile ? "MobileContainer" : "Container"}>
         <div className="TitleBox">
-          <img src={potrait} style={{ width: 80, height: 80 }} alt="Ankita" />
-          <p className="Title">Ankita Panigrahi</p>
+          <img
+            src={potrait}
+            style={{ width: isMobile ? 40 : 80, height: isMobile ? 40 : 80 }}
+            alt="Ankita"
+          />
+          {!isMobile && <p className="Title">Ankita Panigrahi</p>}
         </div>
-        <div className="Container Navbar">
-          <div class="hover-underline-animation">About</div>
+        <div
+          className={
+            isMobile ? "MobileContainer MobileNavbar" : "Container Navbar"
+          }
+        >
           <div class="hover-underline-animation">Work Experience</div>
           <div class="hover-underline-animation">Download Resume</div>
           <div class="hover-underline-animation">Hobbies</div>
           <div class="hover-underline-animation">Contact</div>
         </div>
       </div>
-      <div className="Content">
+      <div className={isMobile ? "MobileContent" : "Content"}>
         <p>
           A Frontend focused Web Developer building the Frontend of Websites and
           Web Applications that leads to the success of the overall product
         </p>
       </div>
-      <div className="Socials">
-        <a href='https://www.linkedin.com/in/ankita-panigrahi-4654917a/' target='_blank' rel="noreferrer">
-          <img src={LinkedIN} alt='linkedIn'/>
+      <div className={isMobile ? "MobileSocials" : "Socials"}>
+        <a
+          href="https://www.linkedin.com/in/ankita-panigrahi-4654917a/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={LinkedIN} alt="linkedIn" />
         </a>
-        <a href="https://github.com/ankita24" target='_blank' rel="noreferrer">
-        <img src={Github} alt='github'/>
+        <a href="https://github.com/ankita24" target="_blank" rel="noreferrer">
+          <img src={Github} alt="github" />
         </a>
-        <a href="https://twitter.com/ankita24_riya" target='_blank' rel="noreferrer">
-        <img src={Twitter} alt='twitter'/>
+        <a
+          href="https://twitter.com/ankita24_riya"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={Twitter} alt="twitter" />
         </a>
       </div>
-      {/* 
-      <div className="FlexBox">
-        <div className="Element">ReactJS</div>
-        <div className="Element">Javascript</div>
-        <div className="Element">HTML</div>
-        <div className="Element">CSS</div>
-        <div className="Element">Typescript</div>
-        <div className="Element">NextJS</div>
-        <div className="Element">Git</div>
+
+      <div className={isMobile ? "MobileFlexBox" : "FlexBox"}>
+        <div className={isMobile ? "MobileElement" : "Element"}>ReactJS</div>
+        <div className="Dot" />
+        <div className={isMobile ? "MobileElement" : "Element"}>Javascript</div>
+        <div className="Dot" />
+        <div className={isMobile ? "MobileElement" : "Element"}>HTML</div>
+        <div className="Dot" />
+        <div className={isMobile ? "MobileElement" : "Element"}>CSS</div>
+        <div className="Dot" />
+        <div className={isMobile ? "MobileElement" : "Element"}>Typescript</div>
+        <div className="Dot" />
+        <div className={isMobile ? "MobileElement" : "Element"}>NextJS</div>
+        <div className="Dot" />
+        <div className={isMobile ? "MobileElement" : "Element"}>Git</div>
       </div>
-      
-      <div style={{ textAlign: "center", marginTop: 50 }}>
-        <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 1 }}>
-          <button className="Button">Work Experience</button>
-        </motion.div>
-      </div> */}
     </div>
   );
 }
---
+++ src/hooks/useCheckPlatform.js
@@ -0,0 +1,18 @@
+import React, { useState,useEffect } from "react";
+
+export default function useCheckPlatform() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  const isMobile = width <= 768;
+  return isMobile;
+}
