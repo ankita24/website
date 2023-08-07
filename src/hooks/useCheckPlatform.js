a8cb1581ba9dde8f8b789238b5f9ec1ee2079dd8
Ankita <riya.ankita24@gmail.com>
2023-08-07 19:25:23 +0530

    unused variable removed

--- src/hooks/useCheckPlatform.js
+++ src/hooks/useCheckPlatform.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import  { useState,useEffect } from "react";
 
 export default function useCheckPlatform() {
   const [width, setWidth] = useState(window.innerWidth);
